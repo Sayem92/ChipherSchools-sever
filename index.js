64fdad85df196bd3c21a193fe0333943d00b0e5c
Sayem92 <sayemmiha123@gmail.com>
2023-04-08 14:44:48 +0600

    add register api with bcrypt

--- index.js
+++ index.js
@@ -22,6 +22,30 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const usersCollection = client.db("ChipherSchools").collection("allUser");
+
+    // ------- Authentication section start --------//
+    // User registration endpoint
+    app.post("/register", async (req, res) => {
+      const { email, password } = req.body;
+      console.log(req.body);
+      // Check if user already exists
+      const user = await usersCollection.findOne({ email });
+      if (user) {
+        return res
+          .status(409)
+          .json({ message: "Email already in used", status: 409 });
+      }
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+      // Insert the new user into the database
+      const result = await usersCollection.insertOne({
+        email,
+        password: hashedPassword,
+      });
+
+      res.status(200).json({ message: "User created", status: 200, result });
+    });
+    
   } finally {
   }
 }
